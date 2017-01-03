1169242683533beacb25928f9be992801ba85a66
Alexanderlau <429095816@qq.com>
2017-01-03 14:22:15 +0800

    spider

---
+++ Douban_movie/Douban_movie/spiders/Douban_movie.py
@@ -0,0 +1,25 @@
+import scrapy
+from scrapy.selector import Selector
+import json
+from ..items import DoubanMovieItem
+
+
+class Douban_Spider(scrapy.Spider):
+    name = 'Movie'
+    start_urls = []
+    for temp in range(0,250,25):
+        url = 'https://movie.douban.com/top250?start=' + str(temp)
+        start_urls.append(url)
+
+    def parse(self, response):
+        sel = Selector(response)
+        movies = sel.xpath('//html/body/div[3]/div[1]/div/div[1]/ol')
+        for movie in movies:
+            item1 = DoubanMovieItem()
+            item1['name'] = movie.xpath('//li/div/div[2]/div[1]/a/span[1]/text()').extract()
+            item1['star'] = movie.xpath('//li/div/div[2]/div[2]/div/span[2]/text()').extract()
+            item1['introduction'] = movie.xpath('//li/div/div[2]/div[2]/p[2]/span/text()').extract()
+            return item1
+
+
+
